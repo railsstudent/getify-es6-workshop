93d76742fbb6dca99d981fa6e73446e7fee660a0
Connie <cleung334@gmail.com>
2017-07-04 07:56:12 +0800

    Complete exercise 5
    Signed-off-by: Connie <cleung334@gmail.com>

--- exercises/ex5/ex5-connie.js
+++ exercises/ex5/ex5-connie.js
@@ -1,5 +1,10 @@
 var numbers = {
 	// ..
+	*[Symbol.iterator]({start = 0, end = 100, step = 1} = {}) {
+		for (let i = start; i <= end; i+= step) {
+			yield i;
+		}
+	}
 };
 
 // should print 0..100 by 1s
@@ -7,7 +12,11 @@ for (let num of numbers) {
 	console.log(num);
 }
 
-// should print 6..30 by 4s
-for (let num of /*..*/) {
+//should print 6..30 by 4s
+for (let num of numbers[Symbol.iterator]({
+	start: 6,
+	end: 30,
+	step: 4
+})) {
 	console.log(num);
 }
